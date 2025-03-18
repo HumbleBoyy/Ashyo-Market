0fe481ade7072925edb4c21ae40c009f8a4fde10
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-18 21:21:33 +0500

    New Commit

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react'
 
 const CustomButton:FC<CustomButtonType> = ({onClick,bage, extraClass, children, icon, iconPosition}) => {
   return (
-    <button onClick={onClick} className={`${extraClass} relative flex py-[14px] px-[25px] text-[16px] bg-[#134E9B] font-semibold rounded-[6px] items-center justify-center gap-[20px]`}>
+    <button onClick={onClick} className={`${extraClass} relative flex py-[14px] px-[25px] text-[16px] bg-[#134E9B] rounded-[6px] items-center justify-center gap-[20px]`}>
       {iconPosition === "left" && icon}
        {children}
       {iconPosition === "right" && icon} 
