30a32b867c3fcb2a18761d53088ea19def8cdecc
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-18 13:32:17 +0500

    New Commit

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,4 +1,4 @@
-import {NextIntlClientProvider, Locale, hasLocale} from 'next-intl';
+import {NextIntlClientProvider, hasLocale} from 'next-intl';
 import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
 import "./globals.css"
@@ -9,7 +9,7 @@ export default async function LocaleLayout({
   children: React.ReactNode;
   params: Promise<{locale: string}>;
 }) {
-  // Ensure that the incoming `locale` is valid
+    
   const {locale} = await params;
   if (!hasLocale(routing.locales, locale)) {
     notFound();
