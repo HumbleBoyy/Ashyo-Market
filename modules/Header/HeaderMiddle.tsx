80d296304f80237a6443353249472cca00bbe869
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-18 16:15:07 +0500

    New Commit

--- modules/Header/HeaderMiddle.tsx
+++ modules/Header/HeaderMiddle.tsx
@@ -1,4 +1,4 @@
-import { ArrowDownIcon, MainLogo, SearchIcon } from "@/assets"
+import { ArrowDownIcon, LibraIcon, LikedIcon, MainLogo, SearchIcon } from "@/assets"
 import CustomButton from "@/components/CustomButton"
 import { Link } from "@/i18n/navigation"
 import { useTranslations } from "next-intl"
@@ -12,14 +12,22 @@ const t = useTranslations('Header')
        <MainLogo/>
        <strong className="text-[#134E9B] text-[36px] font-black">Ashyo</strong>
       </Link>
-      <div className="flex items-center gap-1">
+      <div className="flex items-center gap-[15px]">
         <CustomButton extraClass="text-white cursor-pointer" icon={<ArrowDownIcon/>} iconPosition={"right"}>
           {t("category")}
         </CustomButton>
         <div className="flex items-center w-[518px] h-[53px] bg-[#EBEFF3] rounded-[6px]">
-          <input type="text" placeholder={t("search")} className="w-full px-5 outline-none text-[13px] text-[#00000033]"/>
+          <input type="text" placeholder={t("search")} className="w-full px-5 outline-none text-[15px] text-[#000000]"/>
           <CustomButton extraClass="rounded-l-none text-white h-[53px] cursor-pointer">
              <SearchIcon/>
+          </CustomButton>
+        </div>
+        <div className="flex items-center gap-[10px]">
+          <CustomButton extraClass="bg-[#EBEFF3] !w-[60px] !h-[50px] !p-0 text-[#545D6A] text-center">
+             <LibraIcon/>
+          </CustomButton>
+          <CustomButton extraClass="bg-[#EBEFF3] !w-[60px] !h-[50px] !p-0 text-[#545D6A] text-center">
+             <LikedIcon/>
           </CustomButton>
         </div>
       </div>
