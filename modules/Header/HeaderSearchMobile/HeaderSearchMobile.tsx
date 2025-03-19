5fcbf85a35e35ea8e7ae488dedfd15bda88b2ff7
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-19 15:39:38 +0500

    New Comit

---
+++ modules/Header/HeaderSearchMobile/HeaderSearchMobile.tsx
@@ -0,0 +1,23 @@
+import { ArrowDownIcon, SearchIcon } from '@/assets'
+import CustomButton from '@/components/CustomButton'
+import CustomInput from '@/components/CustomInput'
+import { useTranslations } from 'next-intl'
+
+const HeaderSearchMobile = () => {
+  const t = useTranslations('Header')
+  return (
+    <div className="flex items-center mx-auto  searchDivsmall">
+    <CustomButton extraClass="text-white cursor-pointer" icon={<ArrowDownIcon/>} iconPosition={"right"}>
+      {t("category")}
+    </CustomButton>
+    <div className="flex items-center w-[400px]  xl:w-[518px] h-[53px] bg-[#EBEFF3] rounded-[6px]">
+      <CustomInput type={"text"} placeholder={t("search")}/>
+      <CustomButton extraClass="rounded-l-none text-white h-[53px] cursor-pointer">
+         <SearchIcon/>
+      </CustomButton>
+    </div>
+</div>
+  )
+}
+
+export default HeaderSearchMobile
