6293324c76e3239dc6d24c294ffcc5c71c298067
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-19 15:34:44 +0500

    New Commmit

--- modules/Header/HeaderMiddle.tsx
+++ modules/Header/HeaderMiddle.tsx
@@ -9,17 +9,20 @@ import Menu from "./NavMenu/Menu"
 const HeaderMiddle = () => {
 const t = useTranslations('Header')
   return (
-    <div className='containers flex items-center justify-between py-2 px-2'>
-      <div className="flex items-center gap-0 sm:gap-[10px]">
-        <Link href={"/"} className="flex items-center">
-         <MainLogo/>
-         <strong className="text-[#134E9B] text-[33px] sm:text-[36px] font-black">Ashyo</strong>
-        </Link>
-      </div>
-       <HeaderSearch/>
-       <HeaderNotification/>
-       <Link href={"tel:+998711234567"} className="blcok sm:hidden font-semibold text-[15px] text-[#203F68]">+998 (71) 123-45-67</Link>
-       <Menu/>
+    <div className='containers flex flex-col py-2 px-2'>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-0 sm:gap-[10px]">
+            <Link href={"/"} className="flex items-center">
+            <MainLogo/>
+            <strong className="text-[#134E9B] text-[33px] sm:text-[36px] font-black">Ashyo</strong>
+            </Link>
+          </div>
+            <HeaderSearch/>
+            <HeaderNotification/>
+            <Link href={"tel:+998711234567"} className="blcok sm:hidden font-semibold text-[15px] text-[#203F68]">+998 (71) 123-45-67</Link>
+            <Menu/>
+        </div>
+        <HeaderSearch/>
     </div>
   )
 }
