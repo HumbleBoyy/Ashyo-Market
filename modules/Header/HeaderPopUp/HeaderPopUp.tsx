5ce5709dda4691abb6dc37ca73124f47c57ae82f
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-20 15:16:45 +0500

    New Commit

--- modules/Header/HeaderPopUp/HeaderPopUp.tsx
+++ modules/Header/HeaderPopUp/HeaderPopUp.tsx
@@ -5,7 +5,7 @@ import React, { useContext } from 'react'
 const HeaderPopUp = () => {
     const {openCategory} = useContext(Context)
   return (
-    <div className={`w-full ${openCategory === true ? "border-0 h-0" : "border-1 h-[500px]"}   duration-300 flex absolute top-[100%]`}>
+    <div className={`w-full ${openCategory === true ? "h-0" : "border-1 h-[500px]"}   duration-300 flex absolute top-[100%]`}>
       <div className='w-[30%] bg-[#EBEFF3]'></div>
       <div className='w-[70%] bg-[#FFFFFF]'></div>
     </div>
