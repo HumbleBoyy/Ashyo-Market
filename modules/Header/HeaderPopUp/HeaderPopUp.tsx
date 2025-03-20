cb4c39cbb95f0c460c458f71ca3c1faf1615a218
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-20 15:37:51 +0500

    New Commit

--- modules/Header/HeaderPopUp/HeaderPopUp.tsx
+++ modules/Header/HeaderPopUp/HeaderPopUp.tsx
@@ -5,11 +5,12 @@ import React, { useContext } from 'react'
 
 const HeaderPopUp = () => {
     const {openCategory} = useContext(Context)
-    const {data, isLoading} = getCategory()
-    console.log(data)
+    const {data:categoryList, isLoading} = getCategory()
   return (
     <div className={`w-full ${openCategory === true ? "h-0" : "border-1 h-[500px]"}   duration-300 flex absolute top-[100%]`}>
-      <div className='w-[30%] bg-[#EBEFF3]'></div>
+      <div className='w-[30%] bg-[#EBEFF3]'>
+
+      </div>
       <div className='w-[70%] bg-[#FFFFFF]'></div>
     </div>
   )
