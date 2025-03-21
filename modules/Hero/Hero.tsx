75452b820c2ef39ee9aa80d9d429c08187902734
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-21 16:12:17 +0500

    New Commit

--- modules/Hero/Hero.tsx
+++ modules/Hero/Hero.tsx
@@ -9,9 +9,12 @@ import './styles.css';
 import { BannerType } from "@/types/BannerType";
 import Image from "next/image";
 import { IMAGE_API } from "@/hooks/getEnv";
+import CustomButton from "@/components/CustomButton";
+import { useTranslations } from "next-intl";
 
 const Hero = () => {
     const {data:bannersList} = getBanners()
+    const t = useTranslations("Hero")
   return (
     <div className="w-full bg-[#F3F0F0] h-[450px]">
       <div className="containers">
@@ -36,6 +39,7 @@ const Hero = () => {
                      <div className="flex flex-col items-start text-start gap-[20px]">
                           <h2 className="font-black text-[44px] text-[#0A1729] leading-[120%]">{item.name}</h2>
                           <p className="font-normal text-[16px] text-[#545D6A] leading-[100%]">{item.description}</p>
+                          <CustomButton extraClass={`font-normal text-white text-[16px] leading-[100%]`}>{t("more")}</CustomButton>
                      </div>
                      <Image src={`${IMAGE_API}/${item.image}`} alt={item.name} priority width={750} height={730}/>
                 </div>
