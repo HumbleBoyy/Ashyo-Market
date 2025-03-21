9d8b47efae7f3c916aa32a7538c49f768aafcaba
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-21 14:38:11 +0500

    New Commit

--- modules/Header/HeaderCategory.tsx
+++ modules/Header/HeaderCategory.tsx
@@ -8,7 +8,7 @@ const HeaderCategory = () => {
     const {data:categoryList} = getCategory()
     console.log(categoryList)
   return (
-    <div className='containers w-full'>
+    <div className='containers w-full py-5'>
       <ul className='hidden lg:flex items-center justify-between'>
         {categoryList.map((item:CategoryType)=> <Link className='text-[16px] font-normal text-[#545D6A]' href={"#"} key={item.id}>{item.name}</Link>)}
       </ul>
