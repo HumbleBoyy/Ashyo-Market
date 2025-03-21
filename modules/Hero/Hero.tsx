6385104251ec3ac75eab5cf62086632f541b5d56
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-21 15:31:42 +0500

    New Commit

--- modules/Hero/Hero.tsx
+++ modules/Hero/Hero.tsx
@@ -1,13 +1,22 @@
 "use client"
 import getBanners from "@/service/getBanners"
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './styles.css';
 
 const Hero = () => {
     const {data:bannersList} = getBanners()
-    console.log(bannersList)
   return (
     <div className="w-full bg-[#F3F0F0] h-[450px]">
       <div className="containers">
-
+      <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+      </Swiper>
       </div>
     </div>
   )
