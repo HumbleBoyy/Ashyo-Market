37658d82c58c6b9e903d731823f843d5fc0a6d46
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-21 14:33:37 +0500

    New Commit

--- modules/Header/HeaderCategory.tsx
+++ modules/Header/HeaderCategory.tsx
@@ -1,13 +1,17 @@
 "use client"
+import { Link } from '@/i18n/navigation'
 import getCategory from '@/service/getCategory'
+import { CategoryType } from '@/types/HeaderType'
 import React from 'react'
 
 const HeaderCategory = () => {
     const {data:categoryList} = getCategory()
     console.log(categoryList)
   return (
-    <div className='containers'>
-      HeaderCategory
+    <div className='containers w-full'>
+      <ul className='hidden lg:flex items-center justify-between'>
+        {categoryList.map((item:CategoryType)=> <Link className='text-[16px] font-normal text-[#545D6A]' href={"#"} key={item.id}>{item.name}</Link>)}
+      </ul>
     </div>
   )
 }
