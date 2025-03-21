4836922f9c217c4f364947aae291bd30d91e80b2
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-21 15:59:35 +0500

    New Commit

--- types/BannerType.tsx
+++ types/BannerType.tsx
@@ -3,7 +3,7 @@ import { ProductType } from "./ProductType"
 export interface BannerType{
     id: number
     product_id: number
-    category_id: number | null
+    category_id: number | null | string
     title: string | null
     description:string
     image:string,
