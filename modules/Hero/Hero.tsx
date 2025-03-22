5591344398528be5a1fdcff3488cd6b336087543
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-22 17:12:57 +0500

    New Commit

--- modules/Hero/Hero.tsx
+++ modules/Hero/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
     const {data:bannersList} = getBanners()
     const t = useTranslations("Hero")
   return (
-    <div className="w-full bg-[#F3F0F0] h-[350px] sm:h-[450px]">
+    <div className="w-full bg-[#F3F0F0] hero_content_wrapper h-[450px]">
       <div className="containers relative">
       <Swiper
         spaceBetween={30}
@@ -37,12 +37,12 @@ const Hero = () => {
             <SwiperSlide>
                 <div className="flex items-center justify-between">
                      <div className="absolute left-0 right-0 flex flex-col items-start text-start gap-[20px] ">
-                          <h2 className="w-[60%] font-black text-[22px] sm:text-[44px] text-[#0A1729] leading-[120%]">{item.name}</h2>
-                          <p className="font-normal w-[50%] text-[11px] sm:text-[16px] text-[#545D6A] leading-[100%]">{item.description}</p>
+                          <h2 className="hero_main_title w-[80%]  font-black text-[44px] text-[#0A1729] leading-[120%]">{item.name}</h2>
+                          <p className="hero_main_paragraph font-normal w-[50%] text-[16px] text-[#545D6A] leading-[100%]">{item.description}</p>
                           <CustomButton extraClass={`font-normal text-white text-[16px] leading-[100%] w-[110px] sm:w-[160px] !p-4`}>{t("more")}</CustomButton>
                      </div>
                      <div className="absolute right-0">
-                       <Image className="w-[290px] h-[250px] sm:w-[480px] sm:h-[450px] object-cover" src={`${IMAGE_API}/${item.image}`} alt={item.name} priority width={750} height={730}/>
+                       <Image className="hero_main_Image w-[480px] h-[450px] object-cover" src={`${IMAGE_API}/${item.image}`} alt={item.name} priority width={750} height={730}/>
                      </div>
                 </div>
             </SwiperSlide>
