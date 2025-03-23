ed1652c0da052d1053f7a11f9d4b29a99f3aca18
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-23 16:03:03 +0500

    New Commit

--- service/getBrands.ts
+++ service/getBrands.ts
@@ -2,9 +2,16 @@ import { instance } from "@/hooks/instance"
 import { useQuery } from "@tanstack/react-query"
 
 const getBrands= () => {
+    const extraData = {
+        id:8,
+        name:"Ko'proq",
+        image:"",
+        createdAt: "",
+        updatedAt: ""
+    }
     const {data = [], isLoading} = useQuery({
         queryKey:['brandsAll'],
-        queryFn:()=> instance().get("/brands/all").then(res => res.data)
+        queryFn:()=> instance().get("/brands/all").then(res => [...res.data, extraData])
     })
     
     return {data, isLoading}
