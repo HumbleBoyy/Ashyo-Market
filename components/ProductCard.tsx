48404ef82fcf5e76753f6862d0f8459090cb2984
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-23 22:40:22 +0500

    New Commit

---
+++ components/ProductCard.tsx
@@ -0,0 +1,27 @@
+import { IMAGE_API } from '@/hooks/getEnv'
+import { ProductType } from '@/types/ProductType'
+import Image from 'next/image'
+import React, { FC } from 'react'
+import { FaRegHeart } from 'react-icons/fa'
+
+const ProductCard:FC<{item:ProductType}> = ({item}) => {
+  return (
+    <div className='w-[270px] flex flex-col items-start gap-[15px]'>
+        <div className='relative flex justify-center items-center rounded-[6px] w-full h-[270px] bg-[#EBEFF3]'>
+           <Image className='w-[200px] h-[200px] object-cover' src={`${IMAGE_API}/${item.image}`} alt={item.name} width={200} height={200} priority/>
+           <div className='absolute text-[24px] top-[10px] right-[20px]'>
+               <FaRegHeart color='#545D6A'/>
+           </div>
+        </div>
+        <p className='text-[14px] text-[#545D6A] line-clamp-2 text-start'>{item.description}</p>
+        <div className='flex items-center gap-5'>
+            <strong className='text-[20px] text-[#0A1729]'>{item.price} usz</strong>
+            <div className='flex items-center gap-[10px]'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProductCard
--- modules/Products/Products.tsx
+++ modules/Products/Products.tsx
@@ -1,15 +1,38 @@
 "use client"
 import getProducts from "@/service/getProducts"
-import "./style.css"
 import { useTranslations } from "next-intl"
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './styles.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { ProductType } from "@/types/ProductType";
+import ProductCard from "@/components/ProductCard";
+
 
 const Products = () => {
     const t = useTranslations('Hero')
     const {data:productsList} = getProducts()
     console.log(productsList)
   return (
-    <div className="containers py-[100px]">
-      <h2 className="text-[32px] font-bold text-start px-[10px]">{t("products")}</h2>
+    <div className="containers py-[100px] px-[10px]">
+      <h2 className="text-[32px] font-bold text-start">{t("products")}</h2>
+      <div className="py-[100px]">
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        className="mySwiper"
+      >
+        {productsList.map((item:ProductType)=> (
+            <SwiperSlide key={item.id}>
+               <ProductCard item={item}/>
+            </SwiperSlide>
+        ))}
+      </Swiper>
+      </div>
     </div>
   )
 }
