46a2900d7ff6660360c09f46fb480b70062d5763
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-23 16:40:14 +0500

    New Commit

--- modules/Brands/Brands.tsx
+++ modules/Brands/Brands.tsx
@@ -3,19 +3,18 @@ import { IMAGE_API } from '@/hooks/getEnv'
 import getBrands from '@/service/getBrands'
 import { BrandType } from '@/types/BrandType'
 import Image from 'next/image'
-import React from 'react'
+import "./style.css"
 
 const Brands = () => {
   const {data:brandsList} = getBrands()
+  console.log(brandsList)
   return (
-    <div className='containers brands_wrapper'>
-       
-          {brandsList.map((item:BrandType)=> (
-            <div className='brand_item' key={item.id}>
-                <Image className='w-[200px] h-[70px] object-contain' src={`${IMAGE_API}/${item.image}`} alt={item.name} priority width={200} height={70}/>
+    <div className='containers brands_wrapper py-[100px]'>
+         {brandsList.map((item:BrandType)=> (
+            <div className='brand_item flex justify-center items-center rounded-[6px] py-[20px]' key={item.id}>
+               {!item.image ? <strong className='text-[16px] text-[#134E9B]'>{item.name}</strong> : <Image className='w-[200px] h-[70px] object-contain' src={`${IMAGE_API}/${item.image}`} alt={item.name}width={200} height={70} priority />}
             </div>
           ))}
-       
     </div>
   )
 }
