dd275ae2634ad9459caebf131dacb67635f07e0c
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-24 00:02:45 +0500

    New Commit

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,14 @@
 import Brands from "@/modules/Brands/Brands"
 import Hero from "@/modules/Hero/Hero"
 import Products from "@/modules/Products/Products"
+import ProductsSecond from "@/modules/Products/ProductsSecond"
 const page = () => {
   return (
     <>
       <Hero/>
       <Brands/>
       <Products/>
+      <ProductsSecond/>
     </>
   )
 }
---
+++ modules/Products/ProductsSecond.tsx
@@ -0,0 +1,48 @@
+"use client"
+import getProducts from "@/service/getProducts"
+import { useTranslations } from "next-intl"
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './styles.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { ProductType } from "@/types/ProductType";
+import ProductCard from "@/components/ProductCard";
+
+
+const ProductsSecond = () => {
+    const t = useTranslations('Hero')
+    const {data:productsList} = getProducts()
+  return (
+    <div className="py-[100px] px-[10px]">
+      <div className="containers">
+        <h2 className="text-[25px] sm:text-[32px] font-bold text-start">{t("productsSecond")}</h2>
+      </div>
+        <div className="py-[100px] carousel">
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            loop={true}
+            pagination={{
+              clickable: true,
+            }}
+            className="mySwiper"
+          >
+            {productsList.map((item:ProductType)=> (
+                <SwiperSlide key={item.id}>
+                  <ProductCard item={item}/>
+                </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+
+        <div className="containers products">
+           {productsList.map((item:ProductType)=> (
+             <ProductCard item={item} key={item.id}/>
+           ))}
+        </div>
+    </div> 
+  )
+}
+
+export default ProductsSecond
