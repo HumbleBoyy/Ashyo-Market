53ffab68d0c44a1ada60c9ad9b17247e0dbffd32
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-23 15:58:52 +0500

    New Commit

--- modules/Brands/Brands.tsx
+++ modules/Brands/Brands.tsx
@@ -1,9 +1,21 @@
+"use client"
+import { IMAGE_API } from '@/hooks/getEnv'
+import getBrands from '@/service/getBrands'
+import { BrandType } from '@/types/BrandType'
+import Image from 'next/image'
 import React from 'react'
 
 const Brands = () => {
+  const {data:brandsList} = getBrands()
   return (
-    <div className='h-[100vh]'>
-      
+    <div className='containers brands_wrapper'>
+       
+          {brandsList.map((item:BrandType)=> (
+            <div className='brand_item' key={item.id}>
+                <Image className='w-[200px] h-[70px]' src={`${IMAGE_API}/${item.image}`} alt={item.name} priority width={200} height={70}/>
+            </div>
+          ))}
+       
     </div>
   )
 }
