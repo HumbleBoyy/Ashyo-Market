0125554c12ed2ee588d40d5fc8a578e344b0fd85
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-23 18:05:14 +0500

    New Commit

--- modules/Products/Products.tsx
+++ modules/Products/Products.tsx
@@ -1,13 +1,15 @@
 "use client"
 import getProducts from "@/service/getProducts"
 import "./style.css"
+import { useTranslations } from "next-intl"
 
 const Products = () => {
+    const t = useTranslations('Hero')
     const {data:productsList} = getProducts()
     console.log(productsList)
   return (
     <div className="containers py-[100px]">
-      <h2></h2>
+      <h2 className="text-[32px] font-bold text-start px-[10px]">{t("products")}</h2>
     </div>
   )
 }
