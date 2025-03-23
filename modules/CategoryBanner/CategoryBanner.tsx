63a68098dce92e3c41cbae38b756768c3c7f8275
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-24 02:21:10 +0500

    New Commit

--- modules/CategoryBanner/CategoryBanner.tsx
+++ modules/CategoryBanner/CategoryBanner.tsx
@@ -8,24 +8,16 @@ import "./style.css"
 const CategoryBanner = () => {
     const {data:categoriesBanner} = getCategory(6)
     const t = useTranslations("Hero")
-
-    const moreObj:CategoryType = {
-        id: 78,
-        name:t("more"),
-        image:"",
-        icon:"",
-        parentCategoryId: null,
-        createdAt:"",
-        updatedAt:"",
-        subCategories: []
-    }
-    console.log([...categoriesBanner, moreObj])
   return (
     <div className='containers category_wrapper py-[100px] px-[2px]'>
-        {[...categoriesBanner, moreObj].map((item:CategoryType)=> (
-            <div className='category_item flex justify-center items-center rounded-[6px] py-[20px]' key={item.id}>
-               {!item.image ? <strong className='text-[16px] text-[#134E9B] moreText'>{item.name}</strong> : <Image className='w-[250px] h-[250px] object-contain items-end' src={`${IMAGE_API}/${item.image}`} alt={item.name}width={200} height={70} priority />}
+        {...categoriesBanner.map((item:CategoryType)=> (
+            <>
+            <div className='category_item relative  flex justify-center items-center rounded-[6px] py-[20px]' key={item.id}>
+               {!item.image ? <strong className='text-[16px] text-white moreText'>{item.name}</strong> : <Image className='w-[300px] h-[200px] object-contain items-end' src={`${IMAGE_API}/${item.image}`} alt={item.name}width={200} height={70} priority />}
+               <button className='absolute text-[14px] p-[4px] border-[1px] top-[5px] left-[5px] rounded-[30px] border-white bg-transparent text-white text-center'>{item.name}</button>
             </div>
+            
+            </>
          ))}
     </div>
   )
