d199cf7e2ea1437505d6a5d99f6a40c673e265a0
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-25 16:07:08 +0500

    New Commit

--- app/[locale]/products/[id]/page.tsx
+++ app/[locale]/products/[id]/page.tsx
@@ -15,7 +15,7 @@ const Product = () => {
     <div className='containers py-[50px]'>
        <h2 className='font-bold text-[32px]'>{singleData.name}</h2>
        <div className='flex gap-[32px]'>
-           <div className='relative flex justify-center items-center w-[530px] h-[430px] bg-[#EBEFF3] rounded-[10px]'>
+           <div className='relative flex justify-center items-center mt-[30px] w-[530px] h-[430px] bg-[#EBEFF3] rounded-[10px]'>
               <Image src={`${IMAGE_API}/${singleData.image}`} alt={`${singleData.name}`} width={340} height={340} priority/>
               <div className='absolute text-[24px] text-[#5F728B] font-light top-[25px] right-[30px] flex gap-[20px]'>
                 <FaScaleUnbalanced className='cursor-pointer'/>
