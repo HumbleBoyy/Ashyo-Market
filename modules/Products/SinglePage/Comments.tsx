a364e46da56c28370137529c26befdab5a02e124
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-27 16:22:16 +0500

    New Commit

--- modules/Products/SinglePage/Comments.tsx
+++ modules/Products/SinglePage/Comments.tsx
@@ -1,30 +1,41 @@
 import { ReactNode } from "react"
-import UserImage from "../../../public/user.jpg"
-import Image from "next/image";
+import { UserIconC } from "@/assets";
 
 const Comments = () => {
   const comments:{
-    id: number;
-    userName: string;
-    userImage: ReactNode;
-    star: string;
-    comment: string;
-    commentDate: string;
+    id: number
+    userName: string
+    userImage?: ReactNode
+    star: string
+    comment: string
+    commentDate: string
   }[] = [
     {
       id:1,
       userName:"John Paul",
-      userImage:<Image src={UserImage} alt="UserImage" priority width={50} height={50}/>,
+      userImage:<UserIconC/>,
       star:"⭐⭐⭐⭐⭐",
-      comment:"Good",
+      comment:"The most inconvenient application written with the left heel. The interface is awkward. Putting something up for sale is as difficult as possible. You need to go in the tab in the masonry in the hidden tabs in the buttons. Kick-ass",
       commentDate:"July 18 2024"
     }
   ]
   return (
     <>
       {comments.map((item)=> (
-       <div key={item.id} className="flex items-center gap-[10px]">
-          
+       <div key={item.id} className="flex  gap-[10px]">
+        <div className="flex items-center bg-slate-200 justify-center max-w-[60px] h-[60px] rounded-[100%]">
+          {item.userImage}
+        </div>
+          <div className="flex flex-col gap-[15px]">
+             <div className="flex flex-col">
+                  <h2 className="font-normal text-[18px]">{item.userName}</h2>
+                  <p className="text-[15px]">{item.star}</p>
+             </div>
+             <div className="flex flex-col gap-[10px]">
+                <p className="font-normal text-[12px] text-[#00000066]">{item.commentDate}</p>
+                <p className="text-[15px] font-normal text-[#515D6C] w-[500px]">{item.comment}</p>
+             </div>
+          </div>
        </div>
       ))}
     </>
