1802f44f111dbc56266f7be4b9c962795913394c
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-27 16:48:40 +0500

    New Commit

--- modules/Products/SinglePage/Features.tsx
+++ modules/Products/SinglePage/Features.tsx
@@ -9,7 +9,7 @@ const Features:FC<{id:string | any}> = ({id}) => {
    const {data} = getFeatures(id)
     return (
       <div className="w-full max-w-lg bg-white">
-        {data.cconfigurations ? data?.configurations?.map((item:any) => (
+        {data.cconfigurations !== null ? data?.configurations?.map((item:any) => (
           <div
             key={item.id}
             className="flex justify-between py-2 border-b border-dashed border-gray-300 last:border-b-0"
@@ -17,11 +17,12 @@ const Features:FC<{id:string | any}> = ({id}) => {
             <span className="font-medium text-gray-600">{item.variationOption.variation.name}</span>
             <span className="text-gray-800">{item.variationOption.value}</span>
           </div>
-        )) : <>
+        )) : 
+        <>
          <div className="flex max-w-lg justify-start items-start h-auto">
             <h2 className="text-[18px] w-[50%] font-bold"><span className="text-red-600">!</span>{t("featuresDetail")}</h2>
          </div>
-        </>}
+        </> }
       </div>
     );
   };
