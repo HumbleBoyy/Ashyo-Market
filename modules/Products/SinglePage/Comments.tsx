0046d2e44def3e3208b6d777a34f89b5bf0ea4f6
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-27 16:25:56 +0500

    New Commit

--- modules/Products/SinglePage/Comments.tsx
+++ modules/Products/SinglePage/Comments.tsx
@@ -17,10 +17,26 @@ const Comments = () => {
       star:"⭐⭐⭐⭐⭐",
       comment:"The most inconvenient application written with the left heel. The interface is awkward. Putting something up for sale is as difficult as possible. You need to go in the tab in the masonry in the hidden tabs in the buttons. Kick-ass",
       commentDate:"July 18 2024"
+    },
+    {
+      id:2,
+      userName:"Tomy Kane",
+      userImage:<UserIconC/>,
+      star:"⭐⭐⭐⭐⭐",
+      comment:"The most inconvenient application written with the left heel. You need to go in the tab in the masonry in the hidden tabs in the buttons. Kick-ass",
+      commentDate:"July 18 2024"
+    },
+    {
+      id:3,
+      userName:"Larry Won",
+      userImage:<UserIconC/>,
+      star:"⭐⭐⭐⭐",
+      comment:"You need to go in the tab in the masonry in the hidden tabs in the buttons. Kick-ass",
+      commentDate:"July 18 2024"
     }
   ]
   return (
-    <>
+    <div className="flex flex-col gap-[50px] h-auto overflow-y-hidden">
       {comments.map((item)=> (
        <div key={item.id} className="flex  gap-[10px]">
         <div className="flex items-center bg-slate-200 justify-center max-w-[60px] h-[60px] rounded-[100%]">
@@ -38,7 +54,7 @@ const Comments = () => {
           </div>
        </div>
       ))}
-    </>
+    </div>
   )
 }
 
