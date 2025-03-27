53b5cc2e3d75f4edd8cd3acff3c0b0a1db52421a
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-27 16:03:24 +0500

    New Commit

--- modules/Products/SinglePage/Comments.tsx
+++ modules/Products/SinglePage/Comments.tsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import { ReactNode } from "react"
+import UserImage from "../../../public/user.jpg"
+import Image from "next/image";
 
 const Comments = () => {
-  const comments = [
+  const comments:{
+    id: number;
+    userName: string;
+    userImage: ReactNode;
+    star: string;
+    comment: string;
+    commentDate: string;
+  }[] = [
     {
-      id:1
+      id:1,
+      userName:"John Paul",
+      userImage:<Image src={UserImage} alt="UserImage" priority width={50} height={50}/>,
+      star:"⭐⭐⭐⭐⭐",
+      comment:"Good",
+      commentDate:"July 18 2024"
     }
   ]
   return (
-    <div>
-      Comments
-    </div>
+    <>
+      {comments.map((item)=> (
+       <div key={item.id} className="flex items-center gap-[10px]">
+          
+       </div>
+      ))}
+    </>
   )
 }
 
