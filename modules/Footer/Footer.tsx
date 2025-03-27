6c200043d4e1b4bd481c52cc1cd557ad805215df
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-27 19:16:34 +0500

    New Commit

--- modules/Footer/Footer.tsx
+++ modules/Footer/Footer.tsx
@@ -74,33 +74,33 @@ const Footer = () => {
    </footer>
    <footer className='small_screen_footer containers flex flex-col w-full gap-[20px] py-[100px] px-[20px]'>
            <div className="flex flex-col   gap-[20px]">
-                <h2 className="text-[20px] text-[#000000B2] font-bold">{t("download")}</h2>
+                <h2 className="footer_titles text-[20px] text-[#000000B2] font-bold">{t("download")}</h2>
                 <div className="flex items-center  w-full gap-[10px]">
-                   <button className="flex justify-center items-center gap-[12px] text-[16px] font-bold rounded-[7px] w-[250px] h-[65px] cursor-pointer bg-[#EBEFF3]"><AppStore/>App Store</button>
-                   <button className="flex justify-center items-center gap-[12px] text-[16px] font-bold rounded-[7px] w-[250px] h-[65px] cursor-pointer bg-[#EBEFF3]"><Play/>Google Play</button>
+                   <button className="footer_buttons flex justify-center items-center gap-[12px] text-[16px] font-bold rounded-[7px] w-[250px] h-[65px] cursor-pointer bg-[#EBEFF3]"><AppStore/>App Store</button>
+                   <button className="footer_buttons flex justify-center items-center gap-[12px] text-[16px] font-bold rounded-[7px] w-[250px] h-[65px] cursor-pointer bg-[#EBEFF3]"><Play/>Google Play</button>
                 </div>
             </div>
 
 
-            <div className="flex">
+            <div className="flex gap-0">
             <div className="flex flex-col gap-[10px]">
-            <h2 className="text-[20px] font-bold">{t("menu")}</h2>
+            <h2 className="footer_titles text-[20px] font-bold">{t("menu")}</h2>
             <ul className="flex flex-col gap-[12px]">
-              <li className="list-none text-[16px] text-[#000000B2] cursor-pointer">{t("about")}</li>
-              <li className="list-none text-[16px] text-[#000000B2] cursor-pointer">{t("terms")}</li>
-              <li className="list-none text-[16px] text-[#000000B2] cursor-pointer w-[60%]">{t("privacy")}</li>
-              <li className="list-none text-[16px] text-[#000000B2] cursor-pointer w-[60%]">{t("returnItem")}</li>
-              <li className="list-none text-[16px] text-[#000000B2] cursor-pointer">{t("contactUs")}</li>
+              <li className="footer_texts list-none text-[16px] text-[#000000B2] cursor-pointer w-[150px]">{t("about")}</li>
+              <li className="footer_texts list-none text-[16px] text-[#000000B2] cursor-pointer w-[150px]">{t("terms")}</li>
+              <li className="footer_texts list-none text-[16px] text-[#000000B2] cursor-pointer w-[250px]">{t("privacy")}</li>
+              <li className="footer_texts list-none text-[16px] text-[#000000B2] cursor-pointer w-[250px]">{t("returnItem")}</li>
+              <li className="footer_texts list-none text-[16px] text-[#000000B2] cursor-pointer">{t("contactUs")}</li>
             </ul>
-            <p className="text-[12px] text-[#00000066]">{t("date")}</p>
+               <p className="text-[12px] text-[#00000066]">{t("date")}</p>
             </div>
            <div className="flex flex-col gap-[20px]">
          <div className="flex flex-col gap-[10px]">
-              <h2 className="text-[20px] text-[#000000B2] font-bold">{t("contact")}</h2>
+              <h2 className="footer_titles text-[20px] text-[#000000B2] font-bold">{t("contact")}</h2>
               <Link href={"tel:+998711234567"} className="phoneNumber font-bold text-[24px] text-[#00000080]">+998 (71) 123-45-67</Link>
             </div>
             <div className="flex flex-col gap-[20px]">
-                <div className="flex items-center gap-[10px]">
+                <div className="socialMedia gap-[10px]">
                     {socialMediaBtn.map((item:{id:number, icon:ReactNode})=> (
                         <button className="py-[10px] px-[12px] rounded-[7px] w-[45px] h-[45px] cursor-pointer bg-[#EBEFF3]" key={item.id}>{item.icon}</button>
                     ))}
