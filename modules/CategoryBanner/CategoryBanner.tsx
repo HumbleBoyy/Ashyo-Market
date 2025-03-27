faddab74529f98912fdc17217b276dc01dba7508
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-27 17:48:19 +0500

    New Commit

--- modules/CategoryBanner/CategoryBanner.tsx
+++ modules/CategoryBanner/CategoryBanner.tsx
@@ -13,7 +13,7 @@ const CategoryBanner = () => {
         {...categoriesBanner.map((item:CategoryType)=> (
             <div className='category_item relative  flex justify-center items-center rounded-[6px] py-[20px]' key={item.id}>
                {!item.image ? <strong className='text-[16px] text-white moreText'>{item.name}</strong> : <Image className='w-[300px] h-[200px] object-contain items-end' src={`${IMAGE_API}/${item.image}`} alt={item.name}width={200} height={70} priority />}
-               <button className='absolute text-[14px] p-[4px] border-[1px] top-[5px] left-[5px] rounded-[30px] border-white bg-transparent text-white text-center'>{item.name}</button>
+               <button className='absolute category_btn text-[14px] p-[4px] border-[1px] top-[5px] left-[5px] rounded-[30px] border-white bg-transparent text-white text-center'>{item.name}</button>
             </div>
          ))}
     </div>
