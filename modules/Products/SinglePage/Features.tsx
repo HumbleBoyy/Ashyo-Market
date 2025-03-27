4df7a653dcb5141095c6de6cf4a28d233688b413
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-27 15:10:14 +0500

    New Commit

--- modules/Products/SinglePage/Features.tsx
+++ modules/Products/SinglePage/Features.tsx
@@ -3,30 +3,30 @@
 import getFeatures from "@/service/getFeatures";
 import { FC } from "react";
 
-const Features:FC<{id:string | unknown}> = ({id}) => {
+const Features:FC<{id:string | any}> = ({id}) => {
    const {data} = getFeatures(id)
    console.log(data)
-    const specs = [
-      { label: "Brend", value: "Vivo" },
-      { label: "CIM kartalar soni", value: "2" },
-      { label: "O'lchamlari", value: "75,09x155,11x8,28" },
-      { label: "Modeli", value: "Redmi T12" },
-      { label: "Akumulyator hajmi", value: "4000 amp" },
-      { label: "Yadrolar soni", value: "8" },
-      { label: "Tezkor xotira RAM", value: "6 GB" },
-      { label: "Ichki xotira ROM", value: "128 GB" },
-      { label: "Protsessori", value: "MediaTek Helio P35 (MT6765)" },
-    ];
+    // const specs = [
+    //   { label: "Brend", value: "Vivo" },
+    //   { label: "CIM kartalar soni", value: "2" },
+    //   { label: "O'lchamlari", value: "75,09x155,11x8,28" },
+    //   { label: "Modeli", value: "Redmi T12" },
+    //   { label: "Akumulyator hajmi", value: "4000 amp" },
+    //   { label: "Yadrolar soni", value: "8" },
+    //   { label: "Tezkor xotira RAM", value: "6 GB" },
+    //   { label: "Ichki xotira ROM", value: "128 GB" },
+    //   { label: "Protsessori", value: "MediaTek Helio P35 (MT6765)" },
+    // ];
   
     return (
       <div className="w-full max-w-lg bg-white">
-        {specs.map((spec, index) => (
+        {data.map((item:any) => (
           <div
-            key={index}
+            key={item.id}
             className="flex justify-between py-2 border-b border-dashed border-gray-300 last:border-b-0"
           >
-            <span className="font-medium text-gray-600">{spec.label}</span>
-            <span className="text-gray-800">{spec.value}</span>
+            <span className="font-medium text-gray-600">{item.name}</span>
+            <span className="text-gray-800">{item.options[0].value}</span>
           </div>
         ))}
       </div>
--- service/getFeatures.ts
+++ service/getFeatures.ts
@@ -1,10 +1,10 @@
 import { instance } from "@/hooks/instance"
 import { useQuery } from "@tanstack/react-query"
 
-const getFeatures = (id:string | undefined) => {
-    const {data = {}, isLoading} = useQuery({
-        queryKey:['variations', id],
-        queryFn:()=> instance().get(`/variations/${id}`).then(res => res.data.items)
+const getFeatures = (id:string | any) => {
+    const {data = [], isLoading} = useQuery({
+        queryKey:['variations_all', id],
+        queryFn:()=> instance().get(`/variations/${id}`).then(res => res.data)
     })
     
     return {data, isLoading}
